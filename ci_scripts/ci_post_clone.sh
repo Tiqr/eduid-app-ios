28b5d7630ea5c7780bb9f4beb3ed384fc02c3106
Peter Havekes <peter@havekes.eu>
2023-03-03 14:35:45 +0100

    prevent empty tags

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 cd /Volumes/workspace/repository/
 pwd
+git fetch --unshallow
 git fetch --tags
-NEW_VERSION=$(git describe --abbrev=0)
+NEW_VERSION=$(git describe --abbrev=0 --tags --always)
 echo "Setting marketing version to $NEW_VERSION"
 sed  -i "" -e "s/\(MARKETING_VERSION = \)[^;]*/\1$NEW_VERSION/" /Volumes/workspace/repository/eduID.xcodeproj/project.pbxproj
 
